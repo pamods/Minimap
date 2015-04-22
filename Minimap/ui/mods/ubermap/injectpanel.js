3bd37eacd7ddc8f5a06042a5d65f6b67ba48fef9
ColaColin <cola_colin@ki.tng.de>
2015-04-22 15:17:29 +0200

    knockout 3.3 + deferred start work on the usage of icons minor other functions

--- Minimap/ui/mods/ubermap/injectpanel.js
+++ Minimap/ui/mods/ubermap/injectpanel.js
@@ -2,6 +2,39 @@ console.log("inject ubermap");
 
 (function() {
 	
+	// TODO optimize the unoptimization by doing fast constant camera movements instead of tons of static pips
+	// at best combine it with the hackdecks from unitcommands
+	var unoptimizeNetworkForPlanet = function(planetId) {
+		var makeHackHoloDeck = function(deckId) {
+			var deck = $('<holodeck class="network_unoptimization"></holodeck>');
+			var size = 1;
+			var yPosition = 1;
+			var xPosition = 1;
+			deck.attr('style', "top: "+yPosition+"px; left: "+xPosition+"px; width: "+size+"px; height: "+size+"px;z-index: -1;position:fixed;");
+			deck.attr('id', deckId);
+			$('body').append(deck);
+			return new api.Holodeck($('#'+deckId), {}, undefined);
+		};
+		
+		var setDeckCamera = function(deck, planet, x, y, z, zoom) {
+			var focusBefore = model.holodeck; // assumes live_game
+			deck.focus();
+			api.camera.lookAt({planet_id: planet, location: {x: x, y: y, z: z}, zoom: zoom});
+			if (focusBefore) {
+				focusBefore.focus();
+			}
+		};
+		
+		var deckA = makeHackHoloDeck("network_unoptimization_planet_"+planetId+"_a");
+		var deckB = makeHackHoloDeck("network_unoptimization_planet_"+planetId+"_b");
+		setTimeout(function() {
+			setDeckCamera(deckA, planetId, 0, 0, 1, "orbital");	
+		}, 5000);
+		setTimeout(function() {
+			setDeckCamera(deckB, planetId, 0, 0, -1, "orbital");
+		}, 5000);
+	};
+	
 	handlers.queryViewportSize = function() {
 		api.panels.ubermap_panel.message('setSize', [window.screen.width, window.screen.height]);
 	};
@@ -17,6 +50,51 @@ console.log("inject ubermap");
 	
 	handlers.changeMinimapZ = function(payload) {
 		$('#ubermap_panel').css('z-index', payload.z);
+	};
+	
+	handlers.runUnitCommand = function(payload) {
+		unitCommands[payload.method].apply(null, payload.arguments);
+	};
+	
+	var oldShowAlertPreview = model.showAlertPreview;
+	model.showAlertPreview = function(target) {
+		var oldLookAt = api.camera.lookAt;
+		api.camera.lookAt = function(target) {
+			oldLookAt(target);
+			// locks the poles for the alerts preview pip
+			api.camera.alignToPole();
+		};
+		oldShowAlertPreview(target);
+		api.camera.lookAt = oldLookAt;
+	};
+	
+	var colorByArmyId = {};
+	var oldServerState = handlers.server_state;
+	handlers.server_state = function(msg) {
+		oldServerState(msg);
+//		if (msg.data.client && msg.data.client.commander && msg.data.client.commander.id && msg.data.client.commander.army) {
+//			commanderId = msg.data.client.commander.id;
+//			commanderArmy = msg.data.client.commander.army.id;
+//		}
+		if (msg.data.armies) {
+			for (var i = 0; i < msg.data.armies.length; i++) {
+				colorByArmyId[msg.data.armies[i].id] = msg.data.armies[i].color;
+			}
+		}
+		
+		handlers.queryArmyColors();
+		
+//		if (msg.data.client.army_id) {
+//			api.panels.minimap_panel.message("setMyArmyId", msg.data.client.army_id);
+//		}
+		
+//		playing = msg.state === "playing";
+//		handlers.queryIsPlaying();
+	};
+	
+	handlers.queryArmyColors = function() {
+		console.log("query army colors called...");
+		api.panels.ubermap_panel.message("setArmyColors", colorByArmyId);
 	};
 	
 }());
--- Minimap/ui/mods/ubermap/ubermap.js
+++ Minimap/ui/mods/ubermap/ubermap.js
@@ -3,12 +3,6 @@ console.log("loaded ubermap.js");
 ko.bindingHandlers.datum = {
 	update : function(element, valueAccessor, allBindings, viewModel, bindingContext) {
 		value = ko.unwrap(valueAccessor());
-		/*
-		if (typeof value === "function") {
-			value = value();
-		}
-		element['__data__'] = value;
-		*/
 		d3.select(element).datum(value);
 	}
 };
@@ -17,6 +11,13 @@ ko.bindingHandlers.d = {
 	update : function(element, valueAccessor, allBindings, viewModel, bindingContext) {
 		value = ko.unwrap(valueAccessor());
 		d3.select(element).attr('d', value);
+	}
+};
+
+ko.bindingHandlers.d3b = {
+	update : function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+		value = ko.unwrap(valueAccessor());
+		d3.select(element).datum(value[0]).attr('d', value[1]);
 	}
 };
 
@@ -30,7 +31,7 @@ ko.bindingHandlers.svguse = {
 ko.bindingHandlers.id = {
 	update : function(element, valueAccessor, allBindings, viewModel, bindingContext) {
 		value = ko.unwrap(valueAccessor());
-		d3.select(element).attr('xlink:href', '#'+value);
+		d3.select(element).attr('id', value);
 	}
 };
 
@@ -42,6 +43,102 @@ loadScript("coui://ui/mods/minimap/unitInfoParser.js");
 loadScript("coui://ui/mods/minimap/alertsManager.js");
 
 $(document).ready(function() {
+	
+	function contains(ar, val) {
+		return ar !== undefined && $.inArray(val, ar) !== -1;
+	}
+	
+	function MemoryDataReceiver(pollTime) {
+		var self = this;
+		var lastUpdate = 0;
+		var minPositionChange = 1.5;
+		var queryActive = false;
+		var noMemoryReaderPollTime = 10000;
+		
+		var currentUnits = {};
+		var addedUnitsListeners = [];
+		var updatedUnitsListeners = [];
+		var removeUnitsListeners = [];
+		
+		var addRemovableListener = function(ar, lis) {
+			ar.push(lis);
+			var index = ar.length - 1;
+			return function() {
+				ar.splice(index, 1);
+			};
+		};
+		
+		self.addUnitAddedListener = function(listener) {
+			return addRemovableListener(addedUnitsListeners, listener);
+		};
+
+		self.addUnitUpdatedListener = function(listener) {
+			return addRemovableListener(updatedUnitsListeners, listener);
+		};
+		
+		self.addUnitRemovedListener = function(listener) {
+			return addRemovableListener(removeUnitsListeners, listener);
+		};
+		
+		var notifyListeners = function(listeners, data) {
+			_.forEach(listeners, function(listener) {
+				listener(data);
+			});
+		};
+
+		var notifyAdd = function(data) {
+			notifyListeners(addedUnitsListeners, data);
+		};
+		
+		var notifyUpdate = function(data) {
+			notifyListeners(updatedUnitsListeners, data);
+		};
+		
+		var notifyRemove = function(data) {
+			notifyListeners(removeUnitsListeners, data);
+		};
+		
+		var refreshData = function() {
+			var startQuery = new Date().getTime();
+			$.getJSON("http://127.0.0.1:8184/pa/updateId/"+lastUpdate+"/minPositionChange/"+minPositionChange, function(data) {
+				lastUpdate = data.updateId;
+				if (data.reset) {
+					notifyRemove(currentUnits);
+					currentUnits = {};
+				}
+				
+				_.forEach(data.addedUnits, function(unit) {
+					currentUnits[unit.id] = true;
+					notifyAdd(unit);
+				});
+				
+				_.forEach(data.updatedUnits, function(unit) {
+					notifyUpdate(unit);
+				});
+				
+				var removeKeys = {};
+				_.forEach(data.removedUnits, function(id) {
+					currentUnits[id] = undefined;
+					removeKeys[id] = true;
+				});
+				notifyRemove(removeKeys);
+				
+				setTimeout(refreshData, Math.max(0, pollTime - (new Date().getTime() - startQuery)));
+			}).fail(function() {
+				console.log("Cannot find memory reader, slowing down polling");
+				setTimeout(refreshData, noMemoryReaderPollTime);
+			});
+		}
+		
+		setTimeout(refreshData, 3000);
+	}
+	
+	var memoryPA = new MemoryDataReceiver(100);
+	
+	var unitSpecMapping = undefined;
+	unitInfoParser.loadUnitTypeMapping(function(mapping) {
+		unitSpecMapping = mapping;
+	});
 	
 	var appendLayoutFields = function(self) {
 		self.minimapsBottomMargin = ko.observable(300);
@@ -126,10 +223,138 @@ $(document).ready(function() {
 		});
 	};
 	
-	function MiniMapModel(p, scene) {
+	function UnitModel(unit) {
 		var self = this;
-		self.width = scene.minimapWidth;
-		self.height = scene.minimapHeight;
+		
+		self.transformComputed = undefined;
+		
+		self.id = ko.observable(unit.id);
+		self.planetId = ko.observable(unit.planetId);
+		self.spec = ko.observable();
+		
+		if (contains(unitSpecMapping[unit.spec], "Commander")) {
+			self.spec("commander");
+		} else {
+			self.spec(unit.spec);
+		}
+		
+		self.selected = ko.computed(function() {
+			return model.selection()[self.id()];
+		});
+		
+		self.borderClass = ko.computed(function() {
+			return self.selected() ? "selected_border" : "unselected_border";
+		});
+		
+		self.army = ko.observable(unit.army);
+		self.armyColor = ko.computed(function() {
+			return model.armyColors()[self.army()];
+		});
+		self.x = ko.observable(unit.x);
+		self.y = ko.observable(unit.y);
+		self.z = ko.observable(unit.z);
+		self.currentHp = ko.observable(unit.currentHp);
+		self.maxHp = ko.observable(unit.maxHp);
+		
+		self.updateData = function(update) {
+			self.planetId(update.planetId);
+			self.x(update.x);
+			self.y(update.y);
+			self.z(update.z);
+			self.currentHp(update.currentHp);
+		};
+		
+	}
+	
+	var appendIconsHandling = function(self) {
+		
+		var createTransformComputed = function(unit) {
+			return ko.computed(function() {
+				var ll = convertToLonLan(unit.x(), unit.y(), unit.z());
+				var projected = self.projection()(ll);
+				var x = projected[0];
+				var y = projected[1];
+				return "translate( " + x + "," + y + "), scale(" + self.sizeMod() * 0.2 + ")";
+			});
+		};
+
+		
+		self.unitMap = {};
+		self.units = ko.observableArray([]);
+		
+		var specIdSrc = 0;
+		self.usedSpecsMap = {};
+		self.specMapNotify = ko.observable();
+		
+		self.unitSpecs = ko.computed(function() {
+			self.specMapNotify();
+			var result = [];
+			_.forEach(self.usedSpecsMap, function(specId, key) {
+				result.push(key);
+			});
+			return result;
+		});
+		
+		var addUnitModel = function(unitm) {
+			unitm.transformComputed = createTransformComputed(unitm);
+			self.units.push(unitm);
+			self.unitMap[unitm.id()] = unitm;
+			if (self.usedSpecsMap[unitm.spec()] === undefined) {
+				self.usedSpecsMap[unitm.spec()] = "spec-"+specIdSrc;
+				specIdSrc++;
+				self.specMapNotify.notifySubscribers();
+			}
+		};
+		
+		self.tryAddUnitModel = function(unitm) {
+			if (unitm.planetId() === self.planet().id) {
+				addUnitModel(unitm);
+				return true;
+			} else {
+				return false;
+			}
+		};
+		
+		self.tryAddUnit = function(unit) {
+			if (self.planet().id === unit.planetId) {
+				var m = new UnitModel(unit);
+				addUnitModel(m);
+				return true;
+			} else {
+				return false;
+			}
+		};
+		
+		memoryPA.addUnitAddedListener(self.tryAddUnit);
+		
+		memoryPA.addUnitUpdatedListener(function(unit) {
+			var m = self.unitMap[unit.id];
+			if (m) {
+				m.updateData(unit);
+				if(unit.planetId !== self.planet().id) {
+					model.addPotentialSpaceUnit(m);
+					self.units.remove(function(u) {
+						return unit.id === u.id();
+					});
+				}
+			}
+		});
+		
+		memoryPA.addUnitRemovedListener(function(ids) {
+			self.units.remove(function(unit) {
+				return ids[unit.id()] === true;
+			});
+			// unit specs are not removed from the defs, that just costs more than it is worth
+		});
+		
+		
+	};
+	
+	function MiniMapModel(p) {
+		var self = this;
+		
+		self.width = model.minimapWidth;
+		self.height = model.minimapHeight;
 		self.planet = ko.observable(p);
 		self.dead = ko.computed(function() {
 			return self.planet().dead;
@@ -142,15 +367,16 @@ $(document).ready(function() {
 				id: "p-id-"+self.planet().id,
 				name: self.name(),
 			};
-			if (scene.mappingData() !== undefined) {
-				var ps = scene.mappingData().planets;
+			if (model.mappingData() !== undefined) {
+				var ps = model.mappingData().planets;
 				for (var i = 0; i < ps.length; i++) {
 					if (ps[i].name === self.name()) {
-						result = ps;
+						result = ps[i];
 						break;
 					}
 				}
 			}
+			console.log(result);
 			return result;
 		});
 		
@@ -159,31 +385,120 @@ $(document).ready(function() {
 		});
 		
 		// rotation is only possible in the x-axis, so north always points up
-		self.rotationX = ko.observable(0);
+		self.rotationX = ko.observable(50);
 		self.rotation = ko.computed(function() {
 			return [self.rotationX(), 0];
 		});
+		self.setRotationByPixels = function(px) {
+			self.rotationX(d3.scale.linear().domain([0, self.width()]).range([-180, 180])(px));
+		};
 		
 		self.projection = ko.computed(function() {
 			var w = self.width();
 			var h = self.height();
 			// TODO this currently is fixed onto one projection. I am not yet sure if I want the options for more back.
-			return d3.geo.winkel3().scale(39*(w/200)).translate([w/2, h/2]).precision(.1);
+			return d3.geo.winkel3().scale(39*(w/200)).translate([w/2, h/2]).precision(.1).rotate(self.rotation());
+		});
+		
+		self.sizeMod = ko.computed(function() {
+			return model.minimapWidth() / 200 * (700 / self.planet().radius);
+		});
+		
+		self.dotSizes = ko.computed(function() {
+			return {
+				"spawns": 2 * self.sizeMod(),
+				"metal": 1.5 * self.sizeMod(),
+				"land": 0.9 * self.sizeMod(),
+				"sea": 0.9 * self.sizeMod(),
+				"others": self.sizeMod()
+			};
 		});
 		
 		var thePath = d3.geo.path();
+		thePath.pointRadius(function(o) {
+			var dSizes = self.dotSizes();
+			if (o && o.properties && o.properties.type) {
+				var t = o.properties.type;
+				if (dSizes[t]) {
+					return dSizes[t];
+				}
+			}
+			return dSizes["others"]; 
+		});
+		
 		self.path = ko.computed(function() {
-			self.projection().rotate(self.rotation());
 			thePath.projection(self.projection());
+			self.dotSizes(); // create dependency
 			return thePath;
 		});
 		
+		self.d3b = ko.computed(function() {
+			return [self.graticule(), self.path()];
+		});
+		
+		self.layers = ko.computed(function() {
+			return _.map(_.filter(['land', 'sea', 'metal', 'spawns'], function(layer) {
+				return self.mappingObject()[layer] !== undefined;
+			}), function(layer) {
+				return {
+					layer: layer,
+					data: self.mappingObject()[layer]
+				};
+			});
+		});
+		
+		self.mousemove = function(data, e) {
+			if (e.altKey) {
+				self.setRotationByPixels(e.offsetX);
+			}
+			self.showPreviewByMapXY(e.offsetX, e.offsetY);
+		};
+		
+		self.showPreviewByMapXY = function(x, y) {
+			var ll = self.projection().invert([x, y]);
+			if (ll) {
+				var c = convertToCartesian(ll[1], ll[0]);
+				api.Panel.message(api.Panel.parentId, 'unit_alert.show_preview', {
+					location: {
+						x: c[0],
+						y: c[1],
+						z: c[2]
+					}, 
+					planet_id: self.planet().id,
+					zoom: 'orbital'
+				});
+			};
+		};
+		
+		self.mouseleave = function(data, e) {
+			api.Panel.message(api.Panel.parentId, 'unit_alert.hide_preview');
+		};
+		
+		var lookAtByMinimapXY = function(x, y) {
+			var ll = self.projection().invert([x, y]);
+			if (ll) {
+				var c = convertToCartesian(ll[1], ll[0]);
+				api.camera.lookAt({planet_id: self.planet().id, location: {x: c[0], y: c[1], z: c[2]}, zoom: "orbital"});
+				api.camera.alignToPole();
+			}
+		};
+		
+		self.lookAtMinimap = function(data, e) {
+			lookAtByMinimapXY(e.offsetX, e.offsetY);
+		};
+		
+		
+		appendIconsHandling(self);
 		
 		console.log("created minimap: "+p.name);
 	}
 	
 	function SceneModel() {
 		var self = this;
+		
+		self.selection = ko.observable({});
+		
+		self.armyColors = ko.observable({});
 		
 		self.mappingData = ko.observable();
 		
@@ -193,6 +508,31 @@ $(document).ready(function() {
 		});
 		
 		self.minimaps = ko.observableArray([]);
+		
+		var spaceUnits = {};
+		var tryPutUnitOnPlanet = function(m) {
+			for (var i = 0; i < self.minimaps().length; i++) {
+				if (self.minimaps()[i].tryAddUnitModel(m)) {
+					return true;
+				}
+			}
+			return false;
+		};
+		self.addPotentialSpaceUnit = function(m) {
+			if (!tryPutUnitOnPlanet(m)) {
+				spaceUnits[m.id()] = m;
+			}
+		};
+		
+		memoryPA.addUnitUpdatedListener(function(unit) {
+			var m = spaceUnits[unit.id];
+			if (m) {
+				m.updateData(unit);
+				if (tryPutUnitOnPlanet(m)) {
+					spaceUnits[unit.id] = undefined;
+				}
+			}
+		});
 		
 		appendLayoutFields(self);
 		
@@ -224,14 +564,13 @@ $(document).ready(function() {
 			if (mapList[payload.name]) {
 				console.log("found minimap data in indexdb, will load key "+mapList[payload.name]);
 				DataUtility.readObject(dbName, mapList[payload.name]).then(function(data) {
+					self.mappingData(data);
 					console.log(data);
-					//initBySystem(data);
-					//processRemovals(payload);
 				});
 			} else if (mapData) {
+				self.mappingData(data);
 				console.log("found minimap data in systems.js");
 				console.log(mapData);
-				//initBySystem(mapData);
 			} else {
 				console.log("No minimap data available for map with name "+payload.name);
 			}
@@ -239,6 +578,17 @@ $(document).ready(function() {
 	}
 	
 	model = new SceneModel();
+	
+	handlers.selection = function(payload) {
+		var newSelection = {};
+		_.forEach(payload.spec_ids, function(ids, spec) {
+			_.forEach(ids, function(id) {
+				newSelection[id] = true;
+			});
+		});
+		
+		model.selection(newSelection);
+	};
 	
 	handlers.celestial_data = function(payload) {
 		console.log(payload);
@@ -251,10 +601,17 @@ $(document).ready(function() {
 		model.parentHeight(size[1]);
 	};
 	
+	handlers.setArmyColors = function(clrs) {
+		console.log("got colors");
+		console.log(clrs);
+		model.armyColors(clrs);
+	};
+	
 	app.registerWithCoherent(model, handlers);
 	ko.applyBindings(model);
 	
 	setTimeout(function() {
 		api.Panel.message(api.Panel.parentId, 'queryViewportSize');
+		api.Panel.message(api.Panel.parentId, 'queryArmyColors');
 	}, 500);
 });
